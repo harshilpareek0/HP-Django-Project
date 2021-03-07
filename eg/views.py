fe914678d8eaf41b90edf188274cc3a65a714490
Harshil Pareek <harshilpareek@Harshils-MacBook-Pro.local>
2021-03-06 20:41:09 -0500

    Finished Google account integration

--- eg/views.py
+++ eg/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the exgame index.")
+
+'''
+class IndexView(generic.ListView):
+    template_name = 'index.html'
+'''
