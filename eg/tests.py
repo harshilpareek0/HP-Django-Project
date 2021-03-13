12e558d733e5a8b4672e4e4234c67af0cd952494
canunlu-cu6fba <cu6fb@virginia.edu>
2021-03-13 18:19:32 -0500

    travis first dummy test 3

--- eg/tests.py
+++ eg/tests.py
@@ -1,11 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-
-
-class FirstTestCase(TestCase):
-    def setUp(self):
-        x = 1
-
-    def test_first_test_case(self):
-        self.assertEqual(1, 1)
