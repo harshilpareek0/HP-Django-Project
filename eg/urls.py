149d802ee17d30a3268d61bc6c756777f344312f
Cris Scruggs <cms3zd@virginia.edu>
2021-03-30 13:25:08 -0400

    Added a profile edit view and form

--- eg/urls.py
+++ eg/urls.py
@@ -1,14 +1,18 @@
 from django.urls import include, path
 from django.views.generic import TemplateView
 from . import views
-from .views import UserEditView
-from .views import Profile
+from .views import UserEditView, PasswordsChangeView
+from django.contrib.auth import views as auth_views
+
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="index.html")),
+    path('', TemplateView.as_view(template_name="index.html"), name = 'index'),
     path('game/', views.index),
     path('accounts/', include('allauth.urls')),
     path('profile/',views.ProfileInputView.as_view(),name='profileinput'),
     path('profile/list', views.ProfileView.as_view(), name='profile'),
+    path('edit_profile', UserEditView.as_view(), name = 'edit_profile'),
+    #path('password/', auth_views.PasswordChangeView.as_view(template_name='change-password.html'), name = 'change_password')
+    path('password/', PasswordsChangeView.as_view(template_name='change-password.html'), name = 'change_password')
 ]
--- eg/views.py
+++ eg/views.py
@@ -2,6 +2,15 @@ from django.http import HttpResponse
 from .models import Profile
 from django.views import generic
 from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
+from django.urls import reverse_lazy
+from .forms import EditProfileForm
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('index')
 
 
 def index(request):
@@ -24,3 +33,11 @@ class ProfileInputView(CreateView):
     model = Profile
     template_name = 'profile_input.html'
     fields = ['username','creation_date','age']
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = 'edit_profile.html'
+    success_url = reverse_lazy('index')
+
+    def get_object(self):
+        return self.request.user
